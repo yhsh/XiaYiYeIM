cfa7e851283b2528e71b7d7dd7e9a93e20c14652
xiayiye5 <13343401268@qq.com>
2019-12-23 16:21:42 +0800

    splash界面实现

---
+++ app/src/main/java/com/yhsh/xiayiyeim/BaseActivity.kt
@@ -0,0 +1,57 @@
+package com.yhsh.xiayiyeim
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/*
+ * Copyright (c) 2020, smuyyh@gmail.com All Rights Reserved.
+ * #                                                   #
+ * #                       _oo0oo_                     #
+ * #                      o8888888o                    #
+ * #                      88" . "88                    #
+ * #                      (| -_- |)                    #
+ * #                      0\  =  /0                    #
+ * #                    ___/`---'\___                  #
+ * #                  .' \\|     |# '.                 #
+ * #                 / \\|||  :  |||# \                #
+ * #                / _||||| -:- |||||- \              #
+ * #               |   | \\\  -  #/ |   |              #
+ * #               | \_|  ''\---/''  |_/ |             #
+ * #               \  .-\__  '-'  ___/-. /             #
+ * #             ___'. .'  /--.--\  `. .'___           #
+ * #          ."" '<  `.___\_<|>_/___.' >' "".         #
+ * #         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       #
+ * #         \  \ `_.   \_ __\ /__ _/   .-` /  /       #
+ * #     =====`-.____`.___ \_____/___.-`___.-'=====    #
+ * #                       `=---='                     #
+ * #     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   #
+ * #                                                   #
+ * #               佛祖保佑         永无BUG            #
+ * #                                                   #
+ */
+
+/**
+ * @author 下一页5（轻飞扬）
+ * 创建时间：2019/12/23 15:57
+ * 个人小站：http://yhsh.wap.ai(已挂)
+ * 最新小站：http://www.iyhsh.icoc.in
+ * 联系作者：企鹅 13343401268
+ * 博客地址：http://blog.csdn.net/xiayiye5
+ * 项目名称：XiaYiYeIM
+ * 文件包名：com.yhsh.xiayiyeim
+ * 文件说明：所有activity的基类
+ */
+abstract class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutResId())
+        init()
+    }
+
+    open fun init() {
+
+    }
+
+    //子类必须实现这个加载布局的方法
+    abstract fun getLayoutResId(): Int
+}
---
+++ app/src/main/java/com/yhsh/xiayiyeim/BaseFragment.kt
@@ -0,0 +1,67 @@
+package com.yhsh.xiayiyeim
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/*
+ * Copyright (c) 2020, smuyyh@gmail.com All Rights Reserved.
+ * #                                                   #
+ * #                       _oo0oo_                     #
+ * #                      o8888888o                    #
+ * #                      88" . "88                    #
+ * #                      (| -_- |)                    #
+ * #                      0\  =  /0                    #
+ * #                    ___/`---'\___                  #
+ * #                  .' \\|     |# '.                 #
+ * #                 / \\|||  :  |||# \                #
+ * #                / _||||| -:- |||||- \              #
+ * #               |   | \\\  -  #/ |   |              #
+ * #               | \_|  ''\---/''  |_/ |             #
+ * #               \  .-\__  '-'  ___/-. /             #
+ * #             ___'. .'  /--.--\  `. .'___           #
+ * #          ."" '<  `.___\_<|>_/___.' >' "".         #
+ * #         | | :  `- \`.;`\ _ /`;.`/ - ` : | |       #
+ * #         \  \ `_.   \_ __\ /__ _/   .-` /  /       #
+ * #     =====`-.____`.___ \_____/___.-`___.-'=====    #
+ * #                       `=---='                     #
+ * #     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   #
+ * #                                                   #
+ * #               佛祖保佑         永无BUG            #
+ * #                                                   #
+ */
+
+/**
+ * @author 下一页5（轻飞扬）
+ * 创建时间：2019/12/23 16:01
+ * 个人小站：http://yhsh.wap.ai(已挂)
+ * 最新小站：http://www.iyhsh.icoc.in
+ * 联系作者：企鹅 13343401268
+ * 博客地址：http://blog.csdn.net/xiayiye5
+ * 项目名称：XiaYiYeIM
+ * 文件包名：com.yhsh.xiayiyeim
+ * 文件说明：所有fragment的基类
+ */
+abstract class BaseFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(getLayoutResId(), null)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    open fun init() {
+        //初始化公共功能，子类也可以覆写此方法
+    }
+
+    //所有fragment必须要实现的加载布局的方法
+    abstract fun getLayoutResId(): Int
+
+}
