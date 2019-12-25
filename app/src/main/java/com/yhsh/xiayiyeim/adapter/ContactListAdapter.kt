d64c0a5296e609ecbb3958b745b3853f9808aea1
xiayiye5 <13343401268@qq.com>
2019-12-25 13:48:10 +0800

    联系人列表长按删除好友的操作

--- app/src/main/java/com/yhsh/xiayiyeim/adapter/ContactListAdapter.kt
+++ app/src/main/java/com/yhsh/xiayiyeim/adapter/ContactListAdapter.kt
@@ -1,11 +1,16 @@
 package com.yhsh.xiayiyeim.adapter
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.yhsh.xiayiyeim.R
 import com.yhsh.xiayiyeim.data.ContactListItem
+import com.yhsh.xiayiyeim.ui.activity.ChatActivity
 import com.yhsh.xiayiyeim.widget.ContactListItemView
+import org.jetbrains.anko.startActivity
 
 /*
  * Copyright (c) 2020, smuyyh@gmail.com All Rights Reserved.
@@ -59,9 +64,36 @@ class ContactListAdapter(
 
     override fun onBindViewHolder(holder: ContractViewHolder, position: Int) {
         val contactListItemView = holder.itemView as ContactListItemView
-        println("打印用户：" + contactListItems[position].userName)
-        isShow = position == 0 || contactListItems[position].firstLetter.toUpperCase() != contactListItems[position - 1].firstLetter.toUpperCase()
+        val userName = contactListItems[position].userName
+        println("打印用户：$userName")
+        isShow =
+            position == 0 || contactListItems[position].firstLetter.toUpperCase() != contactListItems[position - 1].firstLetter.toUpperCase()
         contactListItemView.bindView(contactListItems[position], isShow)
+        //设置联系人的点击事件
+        contactListItemView.setOnClickListener {
+            context.startActivity<ChatActivity>("userName" to userName)
+        }
+        //长按删除好友
+        contactListItemView.setOnLongClickListener {
+            AlertDialog.Builder(context).setMessage(
+                String.format(
+                    context.getString(R.string.delete_friend_message),
+                    userName
+                )
+            ).setTitle(context.getString(R.string.delete_friend_title))
+                .setPositiveButton(context.getString(R.string.cancel), null)
+                .setNegativeButton(
+                    context.getString(R.string.confirm)
+                ) { dialog, which ->
+                    deleteFriend(dialog)
+                }.show()
+            true
+        }
+    }
+
+    //删除好友的方法
+    private fun deleteFriend(dialog: DialogInterface) {
+
     }
 
     class ContractViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
